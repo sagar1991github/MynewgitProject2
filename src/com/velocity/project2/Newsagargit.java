78da5f2290eaf5b481af8dc61db48b56482cae51
sagar <sagar.patil0006@gmail.com>
2023-07-12 11:38:28 +0530

    Added new code

---
+++ src/com/velocity/project2/Newsagargit.java
@@ -0,0 +1,12 @@
+package com.velocity.project2;
+
+public class Newsagargit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello sagar");
+		System.out.println("Welcome to git project");
+		System.out.println("Now making project");
+
+	}
+
+}
